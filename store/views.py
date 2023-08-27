717511c8c970484598fe42aec4eea450aee3693e
Ishita Sharma <88889061+ishita1432@users.noreply.github.com>
2023-08-27 21:41:00 +0530

    Update views.py

--- store/views.py
+++ store/views.py
@@ -18,13 +18,17 @@ def home(request):
     sneakers = Product.objects.filter(type="Sneakers")
     category = Category.objects.all()
     trending_products = Product.objects.filter(trending=True)
+    category_men = 'Men'
     context = {
         'product': product,
         'category': category,
         'trending_products': trending_products,
+        'category_men':category_men,
         'sneakers':sneakers,
+
     }
     return render(request, 'index.html', context)
+
 
 
 def category_view(request, sub_category):
@@ -42,7 +46,6 @@ def filter_products(request):
         amount = request.POST.get('max_price')
         products = Product.objects.filter(selling_price__lte=amount)
         return render(request, 'shop.html', {'products': products,'amount':amount})
-
     return render(request, 'shop.html')
 
 def shop(request):
@@ -54,36 +57,49 @@ def shop_with_category(request,category):
     products = Product.objects.filter(category=cat)
     return render(request, 'shop.html', {'products': products})
 
+
+
 def product_type(request,type):
     products = Product.objects,filter(type=type)
     return render(request, 'shop.html', {'products': products})
 
-def product_view(request,cat_slug,prod_slug):
+def product_view(request,cat_slug,sub_cat_slug,prod_slug):
     category = Category.objects.get(slug=cat_slug)
+    sub_category = SubCategory.objects.get(slug=sub_cat_slug)
     category_men = Category.objects.get(name="Men")
-    related_products = Product.objects.filter(category=category).exclude(slug=prod_slug)
+    related_products = Product.objects.filter(sub_category=sub_category).exclude(slug=prod_slug)
+    size_choices = Product.SIZE_CHOICES
     men_slug = category_men.slug
     products = Product.objects.get(slug=prod_slug,category=category)
-    context = {'products':products,'men_slug':men_slug,'related_products' : related_products}
+    context = {'products':products,'men_slug':men_slug,'related_products' : related_products,'size_choices':size_choices}
     return render(request,'product_view.html',context)
 
 def cart(request):
-    cart_pro = Cart.objects.filter()
-    context = {'cart_product':cart_pro}
-    return render(request,'cart.html',context)
+    if request.user.is_authenticated:
+        cart_pro = Cart.objects.filter(user=request.user)
+        context = {'cart_product':cart_pro}
+        return render(request,'cart.html',context)
+    else:
+        messages.error(request,'Please login to your account!!')
+        return redirect('home')
 
 def wishlist(request):
-    wish_pro = Wishlist.objects.filter()
-    context = {'wish_product':wish_pro}
-    return render(request,'wishlist.html',context)
-    
+    if request.user.is_authenticated:
+        wish_pro = Wishlist.objects.filter(user=request.user)
+        context = {'wish_product':wish_pro}
+        return render(request,'wishlist.html',context)
+    else:
+        messages.error(request,'Please login to your account!!')
+        return redirect('home')
+
+
 def add_to_cart(request):
     if request.user.is_authenticated:
         if request.method == 'POST' and request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        
+
                 prod_id = request.POST.get('product_id')
                 product = get_object_or_404(Product, id=prod_id)
-            
+
                 cart_item = Cart.objects.filter(user=request.user, product=product).first()
                 if cart_item:
                     # Product already exists in the cart, update the quantity
@@ -95,18 +111,26 @@ def add_to_cart(request):
                     cart_item = Cart.objects.create(user=request.user, product=product)
                     return JsonResponse({'message': "Product added successfully."})
         return JsonResponse({'message':'Invalid request'})
-    
+
     else:
         return JsonResponse({'message':'Please login to your account!!'})
 
 
+def orders_view(request):
+    if request.user.is_authenticated:
+        orders = Order.objects.filter(user=request.user)
+        return render(request, 'orders.html', {'orders': orders})
+    else:
+        messages.error(request,'Please login to your account!!')
+        return redirect('home')
+
 def update_quantity(request):
     if request.method == 'POST' and request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        product_id = request.POST.get('product_id')
+        prod_id = request.POST.get('product_id')
         quantity = request.POST.get('quantity')
 
         # Retrieve the cart product and update the quantity
-        cart_product = Cart.objects.get(product_id=product_id)
+        cart_product = Cart.objects.get(product_id=prod_id)
         cart_product.product_quantity = quantity
         cart_product.save()
 
@@ -117,7 +141,7 @@ def update_quantity(request):
         # Return a JSON response indicating failure
         response = {'message': 'Invalid request'}
         return JsonResponse(response, status=400)
-    
+
 @require_POST
 def remove_from_cart(request):
     if request.method == 'POST' and request.headers.get('X-Requested-With') == 'XMLHttpRequest':
@@ -130,9 +154,9 @@ def remove_from_cart(request):
             return JsonResponse({'message': 'Cart item does not exist'}, status=400)
         except Exception as e:
             return JsonResponse({'message': str(e)}, status=500)
-        
 
-        
+
+
 @login_required(login_url='login')
 def update_cart_total(request):
     cart_items = Cart.objects.filter(user=request.user)
@@ -142,12 +166,12 @@ def update_cart_total(request):
     for item in cart_items:
         subtotal+=item.product.selling_price*item.product_quantity
         total_price+=item.product.selling_price*item.product_quantity
-    
+
     if subtotal>=499:
         shipping_price = 0
     elif subtotal<0:
         shipping_price = 40
-    
+
     total_price+=shipping_price
 
     data = {
@@ -159,7 +183,6 @@ def update_cart_total(request):
     return JsonResponse(data)
 
 import json
-
 def order_summary(request):
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
@@ -173,10 +196,10 @@ def order_summary(request):
             shipping_price = 0
         elif subtotal<0:
             shipping_price = 40
-            
+
         total_price+=shipping_price
         user_profile = Profile.objects.filter(user=request.user).first()
-        
+
         context = {
             'cart_items': cart_items,
             'subtotal': subtotal,
@@ -195,7 +218,7 @@ def add_to_wishlist(request):
         if request.method == 'POST' and request.headers.get('X-Requested-With') == 'XMLHttpRequest':
                 prod_id = request.POST.get('product_id')
                 product = get_object_or_404(Product, id=prod_id)
-            
+
                 wishlist_item = Wishlist.objects.filter(user=request.user, product=product).first()
                 if wishlist_item:
                     # Product already exists in the cart, update the quantity
@@ -207,7 +230,7 @@ def add_to_wishlist(request):
                     wishlist_item = Wishlist.objects.create(user=request.user, product=product)
                     return JsonResponse({'message': "Product successfully added to the wishlist."})
         return JsonResponse({'message':'Invalid request'})
-    
+
     else:
         return JsonResponse({'message':'Please login to your account!!'})
 
@@ -223,8 +246,8 @@ def remove_from_wishlist(request):
             return JsonResponse({'message': 'Item does not exist'}, status=400)
         except Exception as e:
             return JsonResponse({'message': str(e)}, status=500)
-        
-        
+
+
 @login_required(login_url="login")
 def checkout(request):
     if request.method=="POST":
@@ -233,7 +256,7 @@ def checkout(request):
             current_user.first_name = request.POST.get('fname')
             current_user.last_name = request.POST.get('lname')
             current_user.save()
-            
+
         if not Profile.objects.filter(user=request.user):
             user_profile = Profile()
             user_profile.user = request.user
@@ -244,7 +267,7 @@ def checkout(request):
             user_profile.country = request.POST.get('country')
             user_profile.pincode = request.POST.get('pincode')
             user_profile.save()
-            
+
         neworder = Order()
         neworder.user = request.user
         neworder.fname = request.POST.get('fname')
@@ -268,7 +291,7 @@ def checkout(request):
             shipping_price = 0
         elif subtotal<0:
             shipping_price = 40
-        
+
         total_price+=shipping_price
         neworder.total_price = total_price
         track_no = 'is'+str(random.randint(11111111,99999999))
@@ -276,7 +299,7 @@ def checkout(request):
             track_no = 'is'+str(random.randint(11111111,99999999))
         neworder.tracking_no=track_no
         neworder.save()
-        
+
         neworderitems = Cart.objects.filter(user=request.user)
         for item in neworderitems:
             OrderItem.objects.create(
@@ -288,15 +311,27 @@ def checkout(request):
             orderproduct = Product.objects.filter(id=item.product_id).first()
             orderproduct.quantity = orderproduct.quantity-item.product_quantity
             orderproduct.save()
-        
+
         Cart.objects.filter(user=request.user).delete()
-        # messages.success(request,"Your order has been placed successfully")
-        
+
+
     return render('checkout.html',{})
 
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
+
+
+
+def view_orders(request):
+    # Fetch orders associated with the currently logged-in user
+    user_orders = Order.objects.filter(user=request.user)
+
+    context = {
+        'orders': user_orders,
+    }
+
+    return render(request, 'orders.html', context)
 
 @csrf_exempt
 def payment(request,total_price):
@@ -313,20 +348,27 @@ def payment(request,total_price):
                             'currency':'inr',
                             'product_data':{
                                 'name':'Pay',
+
+                                # 'unit_amount': items.get_total()*100,
                             },
                             'unit_amount':total_price*100,
-                            
+
                         },
                         'quantity': 1,
+                        # 'name': 'ecommerce',  # You can set a custom name for the line item
+                        # 'amount': int(total_amount * 100),  # Stripe expects amount in cents
+                        # 'currency': 'inr',  # Replace with your currency code
+
+
                     }
-                    
-                    
+
+
                 ],
-                
+
                 mode='payment',
-                success_url='http://localhost:8000',
-                cancel_url='http://localhost:8000',
-                
+                success_url='http://ishitasharma.pythonanywhere.com',
+                cancel_url='http://ishitasharma.pythonanywhere.com',
+
             )
         except Exception as e:
             return HttpResponse(e)
