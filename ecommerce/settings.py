6b63a16233cbe1ee7ff60c717888e9c2f706d879
Ishita Sharma <88889061+ishita1432@users.noreply.github.com>
2023-12-06 19:38:06 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1']
+ALLOWED_HOSTS = ['couture-z9me.onrender.com']
 
 
 # Application definition
